5ea18a48e7cf4113a44cd910647a147af60eee7a
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2024-06-07 14:11:02 +0800

    Update: Move index to api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-const routes = require("./routes");
+const routes = require("../routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
